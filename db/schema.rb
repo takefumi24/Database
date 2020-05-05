9d735b239de22131bc13036b863e1c304b52b77c
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-05 21:53:13 +0900

    usersテーブルに一意性制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505123456) do
+ActiveRecord::Schema.define(version: 20200505124921) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
